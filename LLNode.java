50705a35b065798504f8029ffbc346e273378e12
lugubelenusj <lugubelenusj@gmail.com>
2017-03-22 13:35:34 -0700

    First commit.

---
+++ LLNode.java
@@ -0,0 +1,39 @@
+class LLNode<T extends Comparable<T>>
+{
+
+    public LLNode(T  elem, LLNode<T> next)
+    {
+        _elem = elem;
+        _next = next;
+    }
+
+    public LLNode(T elem)
+    {
+        _elem = elem;
+        _next = null;
+    }
+
+    public T elem()
+    {
+        return _elem;
+    }
+
+    public LLNode<T> next()
+    {
+        return _next;
+    }
+
+    public void setElem(T elem)
+    {
+        _elem = elem;
+    }
+
+    public void setNext(LLNode<T> next)
+    {
+        _next = next;
+    }
+
+    private T _elem;
+    private LLNode<T> _next;
+
+}
---
+++ Sort.java
@@ -0,0 +1,75 @@
+public class Sort
+{
+    
+    static <T extends Comparable<T>> void insertionSort(T[] array, int lowIndex, int highIndex, boolean reversed)
+    {
+        // TODO Fill in method stub
+    }
+        
+   public static <T extends Comparable<T>> void selectionSort(T[] A, int lowIndex, int highIndex, boolean reversed) 
+    {
+       // TODO Fill in method stub  
+    }
+   
+   
+   public static void bucketSort(int[] array, int lowindex, int highindex, boolean reversed) 
+   {
+       // TODO Fill in method stub
+   }
+
+
+   public static <T extends Comparable<T>> LLNode<T> mergeSortLL(LLNode<T> list, boolean reversed) 
+   {    
+       // TODO Fill in method stub
+       return null;
+   }
+
+   public static <T extends Comparable<T>> LLNode<T> insertionSortLL(LLNode<T> list, boolean reversed)
+   {
+       // TODO Fill in method stub
+       return null;    
+   }
+
+
+ 
+   public static <T extends Comparable<T>> LLNode<T> selectionSortLL(LLNode<T> list, boolean reversed)
+   {
+       // TODO Fill in method stub
+       return null;
+   }
+
+   public static <T extends Comparable<T>> LLNode<T> quickSortLL(LLNode<T> list, boolean reversed)
+   {
+       // TODO Fill in method stub
+       return null;
+   }
+
+
+   public static <T extends Comparable<T>> void shellSort(T[] array, int lowindex, int highindex, boolean reversed)
+   {
+       // TODO Fill in method stub       
+   }
+
+   public static <T extends Comparable<T>> void heapSort(T[] array, int lowindex, int highindex, boolean reversed)
+   {
+       // ComparableODO Auto-generated method stub       
+   }
+
+   public static <T extends Comparable<T>> void optimizedQuickSort(T[] array, int lowindex, int highindex, boolean reversed)
+   {
+       // TODO Fill in method stub       
+   }
+
+  
+   public  <T extends Comparable<T>>  void quicksort(T[] array, int lowindex, int highindx, boolean reversed)
+   {
+       // TODO Fill in method stub              
+   }
+   
+   public static void radixSort(String[] A, int lowindex, int highindex, boolean reversed)
+   {
+       // TODO Fill in method stub              
+       
+   }
+    
+}
